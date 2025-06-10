5ade619a4fd2cd24854c714159961290a40b440a
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-06-10 17:53:29 -0300

    create Bloco entity and map to Activity

--- src/main/java/com/example/evento/entities/Atividades.java
+++ src/main/java/com/example/evento/entities/Atividades.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,6 +30,9 @@ public class Atividades {
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
+
+    @OneToMany(mappedBy = "atividade")
+    private Set<Bloco> blocos = new HashSet<>();
 
     public Atividades() {
     }
@@ -71,5 +75,32 @@ public class Atividades {
     public void setPreco(Double preco) {
         this.preco = preco;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Atividades other = (Atividades) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
 
 }
